6cc611b67918feb9ef3b02ee0b7de629002cd442
Rafael Rodriguez S. <Rafael Rodriguez S.@localhost>
2014-03-05 14:12:47 -0530

    Arreglo de ciertos detalles. Comments.

--- Sources/Piezo/Piezo.h
+++ Sources/Piezo/Piezo.h
@@ -23,10 +23,4 @@
 
 byte get_shot();
 
-///#################################################################################
-// Funciones privadas.
-
-byte get_piezoADC();
-byte max(byte a[100]);
-
 #endif /* PIEZO_H_ */
