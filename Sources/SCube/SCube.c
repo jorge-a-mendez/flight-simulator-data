7c8c3d41e3e1334a0e48ab577512c521874ed8f0
jorgeamendezm <jorgeamendezm@gmail.com>
2014-03-05 09:17:28 -0530

    Comienzo de implementacion de modulo SCube

--- Sources/SCube/SCube.c
+++ Sources/SCube/SCube.c
@@ -6,3 +6,38 @@
  */
 
 
+///#################################################################################
+// Funciones publicas.
+
+void calibrateSCube(SCube *cube){
+	
+}
+
+byte* get_posX(){
+	
+}
+
+byte* get_posY(){
+	
+}
+
+byte* get_posZ(){
+	
+}
+
+
+//#################################################################################
+// Funciones privadas.
+
+int16u get_frequency(void* pin){
+	waitOnNegVal(pin,(bool)(*pin));
+	setTimer();
+	waitOnNegVal(pin,(bool)(*pin));
+	waitOnNegVal(pin,(bool)(*pin));
+	stopTimer();					//habria que pasar el timer que sea creado en setTimer
+	getTime();
+}
+
+void waitOnNegValue(void* pin, bool initVal){
+	while((bool)(*pin) == initVal)	
+}
