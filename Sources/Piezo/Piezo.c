b4f080f9db35a9ea03d2886b6d3a2ff81f85d70f
jorgeamendezm <jorgeamendezm@localhost>
2014-03-05 12:44:23 -0530

    Modulo de piezoelectrico implementado.

--- Sources/Piezo/Piezo.c
+++ Sources/Piezo/Piezo.c
@@ -6,3 +6,43 @@
  */
 
 
+///#################################################################################
+// Funciones publicas.
+
+byte get_shot(){
+	byte shotVal = get_piezoADC;
+	if(shotVal < SOFT*MAXVALUE/3){
+		return NOSHOT;
+	}
+	else if((shotVal >= SOFT*MAXVALUE/3) && (shotVal < MEDIUM*MAXVALUE/3)){
+		return SOFT;
+	}
+	else if((shotVal >= MEDIUM*MAXVALUE/3) && (shotVal < HARD*MAXVALUE/3)){
+		return MEDIUM;
+	}
+	else{
+		return HARD;
+	}
+}
+
+///#################################################################################
+// Funciones privadas.
+
+byte get_piezoADC(){
+	byte i;
+	byte adc_measures[BUF_SIZE];
+	ADCSC1 |= PIEZO_CH;
+	for(i = 0; i < BUF_SIZE; i++){
+		while(!ADCSC1_COCO);
+		adc_measures[i] = ADCRL;
+		ADCSC1 = ADCSC1;
+	}
+	return max(adc_measures);
+}
+byte max(byte a[BUF_SIZE]){
+	byte max = a[0], i;
+	for(i = 1; i < BUF_SIZE; i++){
+		if(a[BUF_SIZE > max]) max = a[BUF_SIZE];
+	}
+	return max;
+}
--- Sources/Piezo/Piezo.h
+++ Sources/Piezo/Piezo.h
@@ -8,6 +8,25 @@
 #ifndef PIEZO_H_
 #define PIEZO_H_
 
+//Constantes.
 
+#define NOSHOT		0
+#define SOFT		1u
+#define MEDIUM		2u
+#define HARD		3u
+#define MAXVALUE	255
+#define PIEZO_CH	0x01
+#define BUF_SIZE	100
+
+///#################################################################################
+// Funciones publicas.
+
+byte get_shot();
+
+///#################################################################################
+// Funciones privadas.
+
+byte get_piezoADC();
+byte max(byte a[100]);
 
 #endif /* PIEZO_H_ */
