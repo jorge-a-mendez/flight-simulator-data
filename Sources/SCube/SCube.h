a72bafe5a6f2338b1ca6cf6abac481da10ed4130
jorgeamendezm <jorgeamendezm@gmail.com>
2014-03-26 17:06:23 -0530

    Creados modulos de PE para medicion de periodos.

--- Sources/SCube/SCube.h
+++ Sources/SCube/SCube.h
@@ -10,23 +10,9 @@
 
 #include "types.h"
 
-//Constantes
+	
 
-#define Kf		1			//< Constante para calcular capacitancia (C = Kf/f)
-#define CH_X				//< I/O pins a los que se conectan X,Y,Z			
-#define CH_Y	
-#define CH_Z	
 
-//Valores representativos del SCube
-
-typedef struct struct_SCube{
-	int mx;
-	int bx;
-	int my;
-	int by;
-	int mz;
-	int mb;
-}SCube;
 //Se me ocurre que tal vez se puede aproximar (con suerte) a que los cambios en la
 //capacitancia son lineales. Entonces calibrar podria ser pegar la mano a cada una
 //de las placas y luego al extremo opuesto y fijar pendientes y puntos de corte de
