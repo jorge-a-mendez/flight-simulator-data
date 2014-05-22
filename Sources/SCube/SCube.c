172311cd4010ead88e9a7f1a0935dff39ddf635d
rafars307 <rafars307@localhost>
2014-05-21 21:50:15 -0530

    Commenting. Found the problem with the TxInt.

--- Sources/SCube/SCube.c
+++ Sources/SCube/SCube.c
@@ -1,27 +1,41 @@
-/*
+/*###############################################################################
  * SCube.c
+ *		This module contains the functions used to acquire the data from 
+ *		capacitive sensor to locate position in a 3D space. It measures
+ *		the time of charge and discharge controlling the state of a simple
+ *		digital input/output pin.
+ *		Authors:
+ *			Rafael Rodriguez (rafaelrs307@gmail.com)
+ *			Jorge Mendez (jorgeamendezm@gmail.com)
  *
- */
+ *###############################################################################*/
 
 
 #include "SCube/SCube.h"
 #include "Comm/SerialComm.h"
 #include "Events.h"
 
-//Constantes
+//Constants
 
-#define CH_X			0u						
-#define CH_Y			1u
-#define CH_Z			2u
-#define RESOLUTION		8
-#define TOTAL			17		//< Numero de muestras para dos periodos de 60Hz. (1/30)s / 2ms
-#define __Enter_Critical		asm {SEI}			//< REVISAR ESTA INSTRUCCION
-#define __Exit_Critical			asm {CLI}
-#define INPUT			0
-#define OUTPUT			1
+#define 	CH_X			0u			//< Identifier for the plates. Each one corresponding to an axis.						
+#define 	CH_Y			1u
+#define 	CH_Z			2u
+
+#define 	RESOLUTION		8			//< Resolution for the measurement.
+#define 	TOTAL			17			//< Number of samples needed to take 2 cycles of a 60Hz signal.
+
+#define 	__Enter_Critical		asm {SEI};			
+#define 	__Exit_Critical			asm {CLI};
+
+#define 	INPUT			0			//< Digital pins direction.
+#define 	OUTPUT			1
+
+
+
+
 typedef struct{
-	int32u count[3];				//< Tiempo de cargas y descargas.	
-	int32u total;				//< Total de ciclos medidos.
+	int32u count[3];				//< Time of the charge and discharge cycles.	
+	int32u total;					//< Total cycles measured.
 }__SCube_data;
 
 /*
@@ -30,20 +44,28 @@ typedef struct{
  * 	diferente. TOMAR EN CUENTA COMO POSIBLE BUG.
  */
 
-typedef union{				
+
+typedef union{						//< Union to facilitate the transmission.			
 	int16u period;
 	int8u byte[2];
 }__period;
 
-__SCube_data buf;
-bool datalista;
+
+
+// Global Variables.
+__SCube_data buf;					//< Buffer of the Plates Data.
+bool datalista;						//< Boolean to indicate when the data's ready.
+
+// Private functions.
 
 void reset();
 
 
-///#################################################################################
-// Funciones publicas.
+//##################################################################################
 
+// Public Functions.
+
+//##################################################################################
 
 void init_SCube(){
 	buf.count[0] = 0;
@@ -103,8 +125,11 @@ void send_SCube(){
 	__period period;
 	t.t[0] = panel;
 	t.tam = 4;
-	if (total != TOTAL) return;
-	for(i = 0; i < 2; i++){
+	if (!datalista) return;
+	
+	// Send the data of every plate.
+	
+	for(i = 0; i < 2; i++){ 
 		period.period = (buf.count[i] << RESOLUTION) / buf.total;
 		if(period.byte[0] != 0xFF) 
 			t.t[1] = period.byte[0];
@@ -125,8 +150,11 @@ void send_SCube(){
 	reset();
 }
 
-// Funciones privadas
+// ##################################################################################
 
+//		Private functions implementation.
+
+// ##################################################################################
 void reset() {
 	init_SCube();
 }
